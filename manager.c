cc1ad630366e6bad1ba8608c6214277d3b1d6fb1
kym1410 <21800121@handong.edu>
2021-04-24 18:13:12 +0900

    add gitignore

--- manager.c
+++ manager.c
@@ -37,7 +37,7 @@ int selectDataNo(Product *p, int count){
 //배열데이터를 파일에 저장하는 함수
 void saveData(Product p[], int count){
 	FILE* fp;
-
+	/*
 	//중량 가격 제품명
 	fp= fopen("product.txt","wt");
 
@@ -45,7 +45,7 @@ void saveData(Product p[], int count){
 		if(p[i].price == -1) continue;
 		fprintf(fp,"%d %d %s\n",p[i].weight,p[i].price,p[i].name);
 	}	
-	
+	*/
 	fclose(fp);
 	printf("저장됨!\n");
 }
